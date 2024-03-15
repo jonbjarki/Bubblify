929589933b55fcf7af948fcb2db6202e5d6562db
kristofer111 <kristofers21@ru.is>
2024-03-15 17:01:52 +0000

    bubdle useState fix

--- src/components/Bundles.tsx
+++ src/components/Bundles.tsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react";
 import NavigationBar from "./NavigationBar";
 import { BundleType } from "../types/bubble-types";
 
-let [bundles, setBundles] = useState<BundleType[]>([]);
-useEffect(() => {
-  fetch(`http://localhost:3500/api/bundles`)
-    .then((res) => res.json() as Promise<BundleType[]>)
-    .then((data) => {
-      setBundles(data);
-      console.log(data);
-    })
-    .catch((err) => console.error(err));
-}, []);
+
 
 const Bundles = () => {
+  let [bundles, setBundles] = useState<BundleType[]>([]);
+  useEffect(() => {
+    fetch(`http://localhost:3500/api/bundles`)
+      .then((res) => res.json() as Promise<BundleType[]>)
+      .then((data) => {
+        setBundles(data);
+        console.log(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  
     return <NavigationBar />;
 }
 
