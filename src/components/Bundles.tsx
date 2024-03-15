b5d0f7a751eeb053514d352529fd17705bf5e43c
kristofer111 <kristofers21@ru.is>
2024-03-15 22:38:12 +0000

    bundles

--- src/components/Bundles.tsx
+++ src/components/Bundles.tsx
@@ -1,10 +1,22 @@
 import { useEffect, useState } from "react";
 import NavigationBar from "./NavigationBar";
-import { BundleType } from "../types/bubble-types";
+import { BundleType, BubbleType } from "../types/bubble-types";
+import "../styles/Bundles.css"
 
 
 
 const Bundles = () => {
+  let [bubbles, setBubbles] = useState<BubbleType[]>([]);
+  useEffect(() => {
+    fetch(`http://localhost:3500/api/bubbles`)
+      .then((res) => res.json() as Promise<BubbleType[]>)
+      .then((data) => {
+        setBubbles(data);
+        console.log(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   let [bundles, setBundles] = useState<BundleType[]>([]);
   useEffect(() => {
     fetch(`http://localhost:3500/api/bundles`)
@@ -17,7 +29,35 @@ const Bundles = () => {
   }, []);
 
   
-    return <NavigationBar />;
+    return (
+      <>
+      <NavigationBar />
+      <div id="bundles-container">
+      <h1>Bundles</h1>
+      <ul id="bundles-list">
+        {bundles.map((bundle) => (
+          <li key={String(bundle.id)}>
+
+            <div className="product-img-container">
+              {bubbles.map((product) => (
+                 bundle.items.includes(product.id) ? (
+                  <img src={product.image} alt="" />
+                ) : null
+              ))}
+            </div>
+            <div className="product-info-container">
+
+              <p className="product-name">
+                {bundle.name}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+    </>
+    );
+    
 }
 
 export default Bundles;
