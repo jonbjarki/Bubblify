640c31b91026d3976515aad7f6097429f0946249
kristofer111 <kristofers21@ru.is>
2024-03-18 13:37:45 +0000

    cart item delete funcionality

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -23,15 +23,33 @@ const Cart = () => {
   
   console.log("cartItems:", cartItems);
 
+  const handleDelete = (itemID: string) => {
+    localStorage.removeItem(itemID);
+    window.location.reload();
+  }
+
   return (
-      <>
-          <NavigationBar />
-          <div id="cart">
-              {cartItems.map((item) => (
-                  <p>{item.name}</p>
-              ))}
+    <>
+      <NavigationBar />
+      <div id="cart">
+        <div id="header">
+          <h1>Shopping Cart</h1>
+          <button>Porceed to Checkout</button>
+        </div>
+        {cartItems.map((item) => (
+          <div id="item-container">
+            <img src={item.image} alt="" />
+            <div id="options-container">
+              <p>{item.name}</p>
+              <p>${item.price}</p>
+              <button onClick={() => handleDelete(String(item.id))}>
+                delete
+              </button>
+            </div>
           </div>
-      </>
+        ))}
+      </div>
+    </>
   );
 }
 
